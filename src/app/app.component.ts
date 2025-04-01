e6da8ff623e620b5bfa24413f947f143fd416827
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-04-01 04:25:08 +0200

    the thirs commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { FlowbiteService } from './core/service/Flowbit/flowbit.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./layout/navbar/navbar.component";
-import { FoteerComponent } from "./layout/foteer/foteer.component";
+// import { NavbarComponent } from "./layout/navbar/navbar.component";
+// import { FoteerComponent } from "./layout/foteer/foteer.component";
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FoteerComponent, NgxSpinnerComponent],
+  imports: [RouterOutlet, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -5,43 +5,54 @@ import { ProductsService } from '../../core/service/products/products.service';
 import { CartService } from '../../core/service/cart/cart.service';
 @Component({
   selector: 'app-details',
-  imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  private readonly activatedRoute = inject(ActivatedRoute)
-  private readonly productsService = inject(ProductsService)
-  private readonly cartService = inject (CartService)
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly productsService = inject(ProductsService);
+  private readonly cartService = inject(CartService);
 
-  detailsProduct:Iproduct  | null = null;
+  detailsProduct: Iproduct | null = null;
+
   ngOnInit(): void {
+    // Subscription to the route params
     this.activatedRoute.paramMap.subscribe({
-      next:(p)=>{
-        let idProduct = p.get('id') ?? '';
-
-
-        this.productsService.getspecificProducts( idProduct  ).subscribe({
-          next: (res)=>{
-          //  this.detailsProduct = res.data;
-          this.detailsProduct = res.data;
-          },
-          error:(err)=>{
-            console.log(err);
-          }
-        })
+      next: (params) => {
+        const idProduct = params.get('id') ?? '';  // Get the product ID from the URL
+        if (idProduct) {
+          this.fetchProductDetails(idProduct);  // Fetch product details if ID is valid
+        }
+      },
+      error: (err) => {
+        console.log('Error while reading route params:', err);
       }
-    })
+    });
   }
 
+  fetchProductDetails(idProduct: string): void {
+    // Fetch the product details using the product service
+    this.productsService.getspecificProducts(idProduct).subscribe({
+      next: (response) => {
+        this.detailsProduct = response.data;  // Store the product details
+      },
+      error: (err) => {
+        console.log('Error fetching product details:', err);
+      }
+    });
+  }
 
   addToCart(id: string): void {
+    // Add the product to the cart
     this.cartService.addProductCart(id).subscribe({
-      next:(res)=>{
-        console.log(res)
-      },error:(err)=>{
-        console.log(err);
+      next: (response) => {
+        console.log('Product added to cart:', response);
+        // Optionally: Show a success message to the user
+      },
+      error: (err) => {
+        console.log('Error adding product to cart:', err);
+        // Optionally: Show an error message to the user
       }
-    })
+    });
   }
 }
