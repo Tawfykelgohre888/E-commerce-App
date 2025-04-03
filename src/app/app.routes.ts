1fbe053908655b812e88e97760afa3db1af2e72c
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-04-03 18:41:47 +0200

    home puplic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,24 +6,27 @@ import { logindGuard } from './core/guards/logind.guard';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
-    {path: "", component: AuthLayoutComponent, canActivate:[logindGuard] , children: [
-        // { path: "home", loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent), title: "Home" },
-        { path: "login", loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent), title: "Login" },
-        { path: "register", loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent), title: "Register" },
-            {path: 'forgetPassword', loadComponent: () => import('./pages/forget-psssword/forget-psssword.component').then(m => m.ForgetPssswordComponent),title:'forgetPassword'},
+
+    {
+        path: "", component: AuthLayoutComponent, canActivate: [logindGuard], children: [
+            { path: "login", loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent), title: "Login" },
+            { path: "register", loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent), title: "Register" },
+            { path: "forgetPassword", loadComponent: () => import('./pages/forget-psssword/forget-psssword.component').then(m => m.ForgetPssswordComponent), title: "Forget Password" },
+        ]
+    },
+    
+    {
+        path: "", component: BlankLayoutComponent, children: [
+            { path: "home", loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent), title: "Home" }, // Home بدون authGuard
+            { path: "product", loadComponent: () => import('./pages/product/product.component').then(m => m.ProductComponent), title: "Product", canActivate: [authGuard] },
+            { path: "categories", loadComponent: () => import('./pages/categories/categoris.component').then(m => m.CategorisComponent), title: "Categories", canActivate: [authGuard] },
+            { path: "brands", loadComponent: () => import('./pages/brands/brands.component').then(m => m.BrandsComponent), title: "Brands", canActivate: [authGuard] },
+            { path: "cart", loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent), title: "Cart", canActivate: [authGuard] },
+            { path: "details/:id", loadComponent: () => import('./pages/details/details.component').then(m => m.DetailsComponent), title: "Details", canActivate: [authGuard] },
+            { path: "checkout", loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent), title: "Checkout", canActivate: [authGuard] },
         ]
     },
 
-    {
-        path: "", component: BlankLayoutComponent,canActivate:[authGuard],children: [
-            { path: "home", loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent), title: "Home" },
-            { path: "product", loadComponent: () => import('./pages/product/product.component').then(m => m.ProductComponent), title: "Product" },
-            { path: "categories", loadComponent: () => import('./pages/categories/categoris.component').then(m => m.CategorisComponent), title: "Categories" },
-            { path: "brands", loadComponent: () => import('./pages/brands/brands.component').then(m => m.BrandsComponent), title: "Brands" },
-            { path: "cart", loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent), title: "Cart" },
-            { path: "details/:id", loadComponent: () => import('./pages/details/details.component').then(m => m.DetailsComponent), title: "Details" },
-            { path: "checkout", loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent), title: "Checkout" },
-            { path: "**", loadComponent: () => import('./pages/notfound/notfound.component').then(m => m.NotfoundComponent), title: "Not Found" },
-        ]
-    }
+    // صفحة الـ 404
+    { path: "**", loadComponent: () => import('./pages/notfound/notfound.component').then(m => m.NotfoundComponent), title: "Not Found" }
 ];
